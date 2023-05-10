98744cbba541fe5757e704d73c3bee43c14321db
gabriella-codrington <83388074+gabriella-codrington@users.noreply.github.com>
2023-05-10 19:00:19 +0000

    Update BankingApp.java

--- BankingApp.java
+++ BankingApp.java
@@ -3,15 +3,17 @@ import java.util.*;
 public class BankingApp{
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Java Banking! Thank you for using our app!");
-		System.out.println("Enter your name:");
+		System.out.println("Welcome to Java Banking! Sign in (A) or Create an Account (B)?: ");
 		Scanner scanner = new Scanner(System.in);
-		String name = scanner.next();
+		String signInOrCreateAccount = scanner.next();
+		switch(signInOrCreateAccount){
+			if()
+		}
 		System.out.println("Enter your id:");
 		Scanner scanner1 = new Scanner(System.in);
 		String id = scanner1.next();
 		BankAccount myAccount = new BankAccount(name, id);
-		BankAccount.register();
+		BankAccount.createAccount();
 	}
 }
 
